3bcf36da8a1cc1dac29b347548fc6daeb29a1776
Gurjyoth Singh <161277696+gurjyoth@users.noreply.github.com>
2024-10-18 21:22:01 +0530

    Update jump_game.c

--- C/jump_game.c
+++ C/jump_game.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool canJump(int *nums, int numsSize)
-{
-    int maxReach = 0;
+// Function to determine if you can jump to the end of the array
+bool canJump(int *nums, int numsSize) {
+    int maxReach = 0; // Initialize the maximum reachable index
 
-    for (int i = 0; i < numsSize; i++)
-    {
-        // If we've reached a position that is not reachable, return false
-        if (i > maxReach)
-        {
+    for (int i = 0; i < numsSize; i++) {
+        // If the current index is beyond the maximum reachable index, return false
+        if (i > maxReach) {
             return false;
         }
 
-        // Update the maximum reachable position
+        // Update the maximum reachable index
         maxReach = (i + nums[i] > maxReach) ? (i + nums[i]) : maxReach;
 
-        // If we can reach the end of the array, return true
-        if (maxReach >= numsSize - 1)
-        {
+        // If we can reach or exceed the last index, return true
+        if (maxReach >= numsSize - 1) {
             return true;
         }
     }
 
-    return false;
+    return false; // If we finish the loop without reaching the end, return false
 }
 
-int main()
-{
-    int nums[] = {2, 3, 1, 1, 4}; // Example input array
+int main() {
+    // Example input array
+    int nums[] = {2, 3, 1, 1, 4};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
 
-    if (canJump(nums, numsSize))
-    {
+    // Check if the end of the array can be reached and print the result
+    if (canJump(nums, numsSize)) {
         printf("Yes, you can reach the end of the array!\n");
-    }
-    else
-    {
+    } else {
         printf("No, you cannot reach the end of the array.\n");
     }
 
